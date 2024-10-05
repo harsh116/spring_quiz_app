263888b73fa63e9cc84644f68199f52839aed175
alex <you@example.com>
2024-10-05 19:10:43 +0530

    adding base in bite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+    base: "/spring_quiz_app",
+    plugins: [react()],
 })
